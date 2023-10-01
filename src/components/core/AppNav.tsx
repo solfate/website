3991d9b7d300fcd6593b48a4fe7235e4510f83fe
nickfrosty <nfrostbutter@gmail.com>
2023-10-01 16:20:55 -0400

    refactor: removed nav links

--- src/components/core/AppNav.tsx
+++ src/components/core/AppNav.tsx
@@ -6,8 +6,8 @@ export const AppNav = ({ className = "" }: AppNavProps) => {
   return (
     <nav className={className}>
       <NavLink href="/podcast">Podcast</NavLink>
-      <NavLink href="/discover">Discover</NavLink>
-      <NavLink href="/faucet">Faucet</NavLink>
+      {/* <NavLink href="/discover">Discover</NavLink> */}
+      {/* <NavLink href="/faucet">Faucet</NavLink> */}
       {/* <NavLink href="/extension">Extension</NavLink> */}
     </nav>
   );
