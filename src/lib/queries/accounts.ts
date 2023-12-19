fe5b31816f480ffa85d9a5a609eb26573ad71faf
nickfrosty <nfrostbutter@gmail.com>
2023-12-18 19:51:56 -0500

    feat: added queries

---
+++ src/lib/queries/accounts.ts
@@ -0,0 +1,60 @@
+/**
+ * Library of common queries for managing Accounts
+ * (i.e. those from NextAuth)
+ */
+
+import prisma from "@/lib/prisma";
+import { Account, User } from "@prisma/client";
+import { Account as AuthAccount } from "next-auth";
+
+/**
+ * Get a listing of NextAuth Accounts by their provider account id
+ */
+export async function getAccountByProviderId({
+  provider,
+  providerAccountId,
+}: {
+  provider: Account["provider"];
+  providerAccountId: Account["providerAccountId"];
+}) {
+  return prisma.account.findMany({
+    where: { provider, providerAccountId },
+  });
+}
+
+/**
+ * Get a listing of NextAuth Accounts the desired user
+ */
+export async function getAccountsByUserId({
+  userId,
+}: {
+  userId?: Account["userId"];
+}) {
+  if (!userId) return [];
+  return prisma.account.findMany({
+    where: { userId },
+  });
+}
+
+/**
+ * Create a new NextAuth Account, linking it to a User by its `userId`
+ */
+export async function createAccount({
+  userId,
+  account,
+  data,
+}: {
+  userId: User["id"];
+  account: AuthAccount;
+  /** `profile` data returned by the provider upon successful authentication */
+  data?: object;
+}) {
+  // todo: save the external profile `data` returned from the external authentication provider
+  return prisma.account.create({
+    data: {
+      ...account,
+      // force override the account user connection
+      userId,
+    },
+  });
+}
