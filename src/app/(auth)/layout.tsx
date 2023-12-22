99d67db387769c3204113f01a4d76731ceb8fbf7
nickfrosty <nfrostbutter@gmail.com>
2023-12-22 16:45:47 -0500

    feat: redirect off the auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,29 @@
 import { Metadata } from "next";
 import { SITE } from "@/lib/const/general";
+import { getUserSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: `${SITE.name} - Sign In`,
   description: "" + "",
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getUserSession();
+  if (!!session) {
+    /**
+     * todo: handle smart redirects based on url query params
+     * because of how next-auth works, errored auth attempted via external
+     * providers (like when the user rejects) will redirect back to the sign in
+     * page, where there is likely a better place to redirect them
+     *
+     */
+    redirect("/");
+  }
+
   return <>{children}</>;
 }
