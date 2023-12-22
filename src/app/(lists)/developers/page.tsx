4871ad2716bae618bd87c99df5f37474563395e0
nickfrosty <nfrostbutter@gmail.com>
2023-12-22 12:08:30 -0500

    feat: getting the user session

--- src/app/(lists)/developers/page.tsx
+++ src/app/(lists)/developers/page.tsx
@@ -4,6 +4,8 @@ import { SITE } from "@/lib/const/general";
 import dropEmoji from "@/../public/icons/drop.svg";
 import Image from "next/image";
 import { DeveloperListForm } from "@/components/auth/DeveloperListForm";
+import { getUserSession, groupAccountsByProvider } from "@/lib/auth";
+import { getAccountsByUserId } from "@/lib/queries/accounts";
 
 export const metadata: Metadata = {
   title: `${SITE.name} - Verified Solana Developers`,
@@ -13,7 +15,13 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await getUserSession();
+  // group all the accounts by their `provider` (e.g. twitter, github,
+  const groupedAccounts = await getAccountsByUserId({
+    userId: session?.user.id,
+  }).then((accounts) => groupAccountsByProvider(accounts, true));
+
   return (
     <main className="container min-h-[80vh]">
       <section className="max-w-2xl mx-auto space-y-8 text-center container">
@@ -46,7 +54,7 @@ export default function Page() {
         </p>
       </section>
 
-      <DeveloperListForm />
+      <DeveloperListForm session={session} groupedAccounts={groupedAccounts} />
     </main>
   );
 }
