b94d65889788d7ba1c731294df6bc04b02c71d7c
nickfrosty <nfrostbutter@gmail.com>
2023-12-23 09:02:31 -0500

    refactor: disabled redirect for auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,17 +13,17 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getUserSession();
-  if (!!session) {
-    /**
-     * todo: handle smart redirects based on url query params
-     * because of how next-auth works, errored auth attempted via external
-     * providers (like when the user rejects) will redirect back to the sign in
-     * page, where there is likely a better place to redirect them
-     *
-     */
-    redirect("/");
-  }
+  // const session = await getUserSession();
+  // if (!!session) {
+  //   /**
+  //    * todo: handle smart redirects based on url query params
+  //    * because of how next-auth works, errored auth attempted via external
+  //    * providers (like when the user rejects) will redirect back to the sign in
+  //    * page, where there is likely a better place to redirect them
+  //    *
+  //    */
+  //   redirect("/");
+  // }
 
   return <>{children}</>;
 }
