931eddc8f159feb68fa996fcd58ec0bb5f7c5bf9
nickfrosty <nfrostbutter@gmail.com>
2023-12-27 11:51:58 -0500

    fix: wallet popup only when desired

--- src/app/(lists)/layout.tsx
+++ src/app/(lists)/layout.tsx
@@ -10,9 +10,9 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SolanaProvider>
+    <>
       {children}
       <MarketingFooter />
-    </SolanaProvider>
+    </>
   );
 }
