1e49f40d7297102aa141499d91c05d2421a5ce12
nickfrosty <nfrostbutter@gmail.com>
2024-01-01 22:53:28 -0500

    refactor: tweet as link

--- src/lib/middleware/RedirectMiddleware.ts
+++ src/lib/middleware/RedirectMiddleware.ts
@@ -1,6 +1,6 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { parse } from "@/lib/middleware/utils";
-import { SITE } from "@/lib/const/general";
+import { SITE, TWITTER } from "@/lib/const/general";
 
 export default async function RedirectMiddleware(
   req: NextRequest,
@@ -15,14 +15,29 @@ export default async function RedirectMiddleware(
 
   // define the set list of known redirects
   const SHORT_LINKS = {
-    mint42: "/podcast/42#mint",
+    mint42: "/podcast/42",
   };
 
   // located supported redirects
   if (!!SHORT_LINKS[fullKey as never]) {
-    return NextResponse.redirect(
-      new URL(SHORT_LINKS[fullKey as never], SITE.url),
+    const tweetUrl = new URL("https://twitter.com/intent/tweet");
+    tweetUrl.searchParams.append(
+      "text",
+      `I want to mint ${
+        TWITTER.handle
+      } episodes as NFTs!\nPlease gib @nickfrosty and @jamesrp13 🙏\nLike this one 👇${new URL(
+        SHORT_LINKS[fullKey as never],
+        SITE.url,
+      )}`,
     );
+    tweetUrl.searchParams.append("original_referer", SITE.url);
+    tweetUrl.searchParams.append("related", TWITTER.handle);
+
+    return NextResponse.redirect(new URL(tweetUrl.toString()));
+
+    // return NextResponse.redirect(
+    //   new URL(SHORT_LINKS[fullKey as never], SITE.url),
+    // );
   }
 
   // fallback redirect to main site home page
