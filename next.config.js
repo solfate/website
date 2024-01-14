d06f6d71a413fcbc8e74bb064071ad1d60c28dc7
nickfrosty <nfrostbutter@gmail.com>
2024-01-14 11:25:51 -0500

    feat: package updates

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /**
  *
  */
-const withMDX = require("@next/mdx")({
-  // support *.md and *.mdx
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
 
 const { withContentlayer } = require("next-contentlayer");
 
@@ -91,5 +78,4 @@ const nextConfig = {
   },
 };
 
-// module.exports = withMDX(nextConfig);
 module.exports = withContentlayer(nextConfig);
