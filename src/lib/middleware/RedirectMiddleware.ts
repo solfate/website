7543d0fc888054ce78c39d2f46a59416d9cc94be
nickfrosty <nfrostbutter@gmail.com>
2024-01-15 23:56:16 -0500

    refactor: mint redirect

--- src/lib/middleware/RedirectMiddleware.ts
+++ src/lib/middleware/RedirectMiddleware.ts
@@ -13,33 +13,50 @@ export default async function RedirectMiddleware(
     return NextResponse.redirect(new URL("/", SITE.url));
   }
 
-  // define the set list of known redirects
-  const SHORT_LINKS = {
-    mint42: "/podcast/42",
-    mint43: "/podcast/43",
-  };
+  // todo: this will not support float urls `7.5`
+  const mintableRegex = new RegExp(/^mint(\d+)/i);
+  if (mintableRegex.test(fullKey)) {
+    try {
+      const episode = mintableRegex.exec(fullKey)?.[1];
 
-  // located supported redirects
-  if (!!SHORT_LINKS[fullKey as never]) {
-    const tweetUrl = new URL("https://twitter.com/intent/tweet");
-    tweetUrl.searchParams.append(
-      "text",
-      `I want to mint ${
-        TWITTER.handle
-      } episodes as NFTs on @solana!\nPlease gib @nickfrosty and @jamesrp13 🙏\nLike this one 👇\n${new URL(
-        SHORT_LINKS[fullKey as never],
-        SITE.url,
-      )}`,
-    );
-    tweetUrl.searchParams.append("original_referer", SITE.url);
-    tweetUrl.searchParams.append("related", TWITTER.handle);
+      if (!episode) throw Error("Unknown episode");
 
-    return NextResponse.redirect(new URL(tweetUrl.toString()));
-
-    // return NextResponse.redirect(
-    //   new URL(SHORT_LINKS[fullKey as never], SITE.url),
-    // );
+      // redirect to podcast page with the mint trigger
+      return NextResponse.redirect(
+        new URL(`/podcast/${episode}?mint`, SITE.url),
+      );
+    } catch (err) {}
+    // we already know this is a mint short link, so always go to the podcast page
+    return NextResponse.redirect(new URL("/podcast", SITE.url));
   }
+
+  // // define the set list of known redirects
+  // const SHORT_LINKS = {
+  //   mint42: "/podcast/42",
+  //   mint43: "/podcast/43",
+  // };
+
+  // // located supported redirects
+  // if (!!SHORT_LINKS[fullKey as never]) {
+  //   const tweetUrl = new URL("https://twitter.com/intent/tweet");
+  //   tweetUrl.searchParams.append(
+  //     "text",
+  //     `I want to mint ${
+  //       TWITTER.handle
+  //     } episodes as NFTs on @solana!\nPlease gib @nickfrosty and @jamesrp13 🙏\nLike this one 👇\n${new URL(
+  //       SHORT_LINKS[fullKey as never],
+  //       SITE.url,
+  //     )}`,
+  //   );
+  //   tweetUrl.searchParams.append("original_referer", SITE.url);
+  //   tweetUrl.searchParams.append("related", TWITTER.handle);
+
+  //   return NextResponse.redirect(new URL(tweetUrl.toString()));
+
+  //   // return NextResponse.redirect(
+  //   //   new URL(SHORT_LINKS[fullKey as never], SITE.url),
+  //   // );
+  // }
 
   // fallback redirect to main site home page
   return NextResponse.redirect(new URL("/", SITE.url));
