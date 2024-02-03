effb2fd746cf81fe22dbca29197e07f2a20e8e4a
nickfrosty <nfrostbutter@gmail.com>
2024-02-03 11:28:30 -0500

    refactor: moved toggle to component file

--- src/components/lists/ClaimDevListToken.tsx
+++ src/components/lists/ClaimDevListToken.tsx
@@ -24,9 +24,8 @@ import {
   ApiListDevelopersPutResponse,
 } from "@/types/api/developers";
 import { PulseLoader } from "react-spinners";
-import clsx from "clsx";
-import { FeatherIcon } from "@/components/core/FeatherIcon";
 import { solanaExplorerLink } from "@/lib/solana/helpers";
+import { MetadataToggle } from "./MetadataToggle";
 
 type ClaimDevListTokenProps = {
   twitter?: string;
@@ -486,46 +485,4 @@ export const claimButtonLabel = ({
     default:
       return placeholder;
   }
-};
-
-type MetadataToggleProps = {
-  label: string;
-  value?: string;
-  disabled?: boolean;
-  checked: React.SetStateAction<boolean>;
-  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-export const MetadataToggle = ({
-  label,
-  value,
-  checked,
-  setChecked,
-  disabled,
-}: MetadataToggleProps) => {
-  if (!value) return null;
-
-  return (
-    <button
-      type="button"
-      disabled={disabled}
-      onClick={() => setChecked((prev) => !prev)}
-      className={clsx(
-        "btn block text-left btn-ghost !px-3",
-        checked && "border-green-300 bg-green-100 hover:border-green-500",
-      )}
-    >
-      <span className="flex items-center justify-between gap-2">
-        <span className="block text-xs line-clamp-1 text-gray-500">
-          {label}
-        </span>
-        <FeatherIcon
-          name={checked ? "CheckCircle" : "XOctagon"}
-          size={16}
-          className={checked ? "text-green-700" : "text-gray-400"}
-        />
-      </span>
-      <span className="block text-sm line-clamp-1">{value}</span>
-    </button>
-  );
 };
