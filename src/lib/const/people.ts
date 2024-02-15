cda6708f8bd2f47b6e140bb2633f2a5f7e392729
nickfrosty <nfrostbutter@gmail.com>
2024-02-15 18:22:09 -0500

    refactor: people details

--- src/lib/const/people.ts
+++ src/lib/const/people.ts
@@ -18,10 +18,9 @@ export const SOLFATE_AUTHORS: {
     twitter: "nickfrosty",
     github: "nickfrosty",
     website: "https://nick.af",
-    title: "Host of the Solfate Podcast",
+    title: "Co-founder of Solfate, Host of the Solfate Podcast",
     bio: "Nick is a self taught developer and submariner. He spends his free time \
-    writing docs and technical articles, as well as building side projects. \
-    He strives to one day make a living online from those projects.",
+    writing docs, technical articles, and building various projects in the Solana ecosystem.",
   },
   james: {
     name: "James P.",
@@ -29,7 +28,7 @@ export const SOLFATE_AUTHORS: {
     twitter: "jamesrp13",
     github: "jamesrp13",
     // website: "",
-    title: "Host of the Solfate Podcast",
+    title: "Co-founder of Solfate, Host of the Solfate Podcast",
     bio: "James is still trying to figure out what to do when he grows up, \
     but he's been making a living for almost a decade by creating software \
     and educating devs through his software agency Unboxed. So much for that Econ degree.",
