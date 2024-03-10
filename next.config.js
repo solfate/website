8895d67dd65b08aeed364ee687ffca82c4b6dc9f
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-03-10 12:57:15 -0400

    fix: next redirects vs rewrites

--- next.config.js
+++ next.config.js
@@ -10,65 +10,65 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        permanent: true,
         source: "/developers",
         destination: "/devlist",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/twitter",
         destination: "https://twitter.com/SolfatePod",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/youtube",
         destination: "https://youtube.com/@SolfatePod",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/spotify",
         destination:
           "https://open.spotify.com/show/5YnYJdFDfEM16Om3v4VRcs?si=4SW7C3wNS_eWYz7YCFfdcw",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/apple",
         destination:
           "https://podcasts.apple.com/us/podcast/solfate-podcast/id1663919657",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/pcast",
         destination:
           "https://pca.st/podcast/f7e2e7d0-7293-013b-f273-0acc26574db2",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/pocketcasts",
         destination:
           "https://pca.st/podcast/f7e2e7d0-7293-013b-f273-0acc26574db2",
         permanent: true,
       },
       {
+        permanent: true,
         source: "/youtube",
         destination: "https://youtube.com/@SolfatePod",
         permanent: true,
       },
       {
-        source: "/nickfrosty",
-        destination: "https://twitter.com/nickfrosty",
         permanent: true,
-      },
-      {
         source: "/podcast/nickfrosty",
         destination: "https://twitter.com/nickfrosty",
         permanent: true,
       },
       {
-        source: "/jamesrp13",
-        destination: "https://twitter.com/jamesrp13",
         permanent: true,
-      },
-      {
         source: "/podcast/jamesrp13",
         destination: "https://twitter.com/jamesrp13",
         permanent: true,
