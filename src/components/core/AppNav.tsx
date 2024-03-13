7f934719cfac01ef10f2d641897cdff8a5460234
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-03-12 23:15:48 -0400

    refactor: nav styles

--- src/components/core/AppNav.tsx
+++ src/components/core/AppNav.tsx
@@ -1,3 +1,4 @@
+import styles from "@/styles/Nav.module.css";
 import { NavLink } from "@/components/core/NavLink";
 
 type AppNavProps = SimpleComponentProps;
@@ -5,10 +6,15 @@ type AppNavProps = SimpleComponentProps;
 export const AppNav = ({ className = "" }: AppNavProps) => {
   return (
     <nav className={className}>
-      <NavLink href="/blog">Blog</NavLink>
-      <NavLink href="/podcast">Podcast</NavLink>
-      {/* <NavLink href="/discover">Discover</NavLink> */}
-      <NavLink href="/devlist">DevList</NavLink>
+      <NavLink href="/podcast" className={styles.primaryNavLink}>
+        Podcast
+      </NavLink>
+      <NavLink href="/devlist" className={styles.primaryNavLink}>
+        DevList
+      </NavLink>
+      <NavLink href="/blog" className={styles.primaryNavLink}>
+        Blog
+      </NavLink>
       {/* <NavLink href="/extension">Extension</NavLink> */}
     </nav>
   );
