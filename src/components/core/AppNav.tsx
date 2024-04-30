81d91d4044b6a66cf2b5bbc6af1e45372f323b5e
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-04-30 02:15:57 -0400

    refactor: nav links

--- src/components/core/AppNav.tsx
+++ src/components/core/AppNav.tsx
@@ -11,11 +11,11 @@ type AppNavProps = SimpleComponentProps;
 export const AppNav = ({ className = "" }: AppNavProps) => {
   return (
     <nav className={className}>
+      {/* <NavLink href={ROUTE_PREFIX_DISCOVER}>Discover</NavLink> */}
       <NavLink href={ROUTE_PREFIX_BLOG}>Blog</NavLink>
       <NavLink href={ROUTE_PREFIX_PODCAST}>Podcast</NavLink>
-      {/* <NavLink href={ROUTE_PREFIX_DISCOVER}>Discover</NavLink> */}
-      <NavLink href={ROUTE_PREFIX_DEVLIST}>DevList</NavLink>
       <NavLink href={ROUTE_PREFIX_SNAPSHOT}>Snapshot</NavLink>
+      <NavLink href={ROUTE_PREFIX_DEVLIST}>DevList</NavLink>
     </nav>
   );
 };
