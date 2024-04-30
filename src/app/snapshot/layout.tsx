1e0aa4bddf0933544f878dd40d32e2f550ce39c5
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-04-30 04:03:24 -0400

    feat: social image

--- src/app/snapshot/layout.tsx
+++ src/app/snapshot/layout.tsx
@@ -2,9 +2,15 @@ import MarketingFooter from "@/components/core/MarketingFooter";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: `Solfate Snapshot`,
+  title: `Solfate Snapshot - Solana email newsletter`,
   description:
-    "Byte-sized email newsletter filled with the biggest updates from Solana ecosystem teams and builders. ~5 minute read. Every 2 weeks. Simply free. ☀️📸",
+    "Byte-sized email newsletter filled with the biggest updates " +
+    "from Solana ecosystem teams and builders. " +
+    "~5 minute read. Every 2 weeks. Simply free. ☀️📸",
+  openGraph: {
+    type: "website",
+    images: "/media/social-snapshot.png?6sss",
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
