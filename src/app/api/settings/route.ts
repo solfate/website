fafa2736bae5b18a6852c7aa35164122999a1f65
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-05-03 00:19:43 -0400

    feat: added basic zod

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -4,19 +4,31 @@
 
 import { withUserAuth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
+import { z } from "zod";
 import { ApiSettingsPatchInput } from "@/types/api/social";
+import { usernameSchema } from "@/lib/schema/prisma";
+import { ApiErrorResponse } from "@/lib/api";
 
 export const PATCH = withUserAuth(async ({ req, session }) => {
   try {
-    const input: ApiSettingsPatchInput = await req.json();
-    if (!input) throw "Invalid input";
+    const rawInput: ApiSettingsPatchInput = await req.json();
+    if (!rawInput) throw "Invalid input";
 
-    if (typeof input.username != "undefined") {
-      input.username = input.username.trim();
+    if (
+      typeof rawInput.username != "undefined" &&
+      !!rawInput?.username &&
+      rawInput.username.toLowerCase() == session.user.username.toLowerCase()
+    ) {
+      return new Response("Your username was unchanged");
+    }
 
-      if (input.username == session.user.username)
-        return new Response("Your username was unchanged");
+    const inputSchema = z.object({
+      username: usernameSchema,
+    });
 
+    const input = inputSchema.parse(rawInput);
+
+    if (!!input.username) {
       const isUsernameTaken = await prisma.user.findUnique({
         where: {
           username: input.username,
@@ -41,23 +53,11 @@ export const PATCH = withUserAuth(async ({ req, session }) => {
       });
 
       if (!updatedUser) throw "Unable to update your username";
-
-      // success! yay :)
       return new Response("Your username was updated");
     }
 
-    // catch all error for unhandled requests
-    throw "Unknown settings update request";
+    return new Response("No changes were made");
   } catch (err) {
-    console.warn("[API error]", err);
-
-    let message = "An unknown error occurred";
-
-    if (typeof err == "string") message = err;
-    else if (err instanceof Error) message = err.message;
-
-    return new Response(message, {
-      status: 400,
-    });
+    return ApiErrorResponse(err);
   }
 });
