d3c48405b8f74d944b8c75170fec1fc53dca5987
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-05-11 16:04:32 -0400

    feat: redirect to create profile

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -22,9 +22,10 @@ export default async function Page({ params }: PageProps) {
 
   if (!profile) {
     // direct the user to setup their profile
-    // if (session?.user.username == params.user) {
-    //   return redirect("/onboarding");
-    // }
+    if (session?.user.username == params.user) {
+      return redirect("/settings/profile");
+      // return redirect("/onboarding");
+    }
 
     return notFound();
   }
