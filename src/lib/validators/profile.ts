bfa90383a06c595676325f012c73d864e957a3e9
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-05-22 21:24:20 -0400

    feat: username validation

--- src/lib/validators/profile.ts
+++ src/lib/validators/profile.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
-import { USERNAME_MAX_LEN, USERNAME_MIN_LEN } from "@/lib/const/profile";
+import {
+  USERNAME_BLACKLIST,
+  USERNAME_BLACKLIST_PREFIX,
+  USERNAME_MAX_LEN,
+  USERNAME_MIN_LEN,
+  USERNAME_REGEX,
+} from "@/lib/const/profile";
 
 export const usernameSchema = z
   .string()
@@ -10,4 +16,15 @@ export const usernameSchema = z
   .refine(
     (val) => val.length >= USERNAME_MIN_LEN,
     `Usernames <${USERNAME_MIN_LEN} characters are reserved`,
-  );
+  )
+  .refine(
+    (val) => USERNAME_REGEX.test(val),
+    `Invalid characters in the username`,
+  )
+  .refine((val) => !USERNAME_BLACKLIST.includes(val), `Username not allowed`)
+  .refine((val) => {
+    for (let prefix of USERNAME_BLACKLIST_PREFIX) {
+      if (val.startsWith(prefix)) return false;
+    }
+    return true;
+  }, `Username prefix not allowed`);
