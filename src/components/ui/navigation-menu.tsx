6ae3554ddb2bbf1a09da01d2d05f6ae405f0b2b5
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-05-25 08:27:17 -0400

    feat: added link item

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -4,6 +4,7 @@ import { cva } from "class-variance-authority";
 import { ChevronDown } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
@@ -115,9 +116,39 @@ const NavigationMenuIndicator = React.forwardRef<
 NavigationMenuIndicator.displayName =
   NavigationMenuPrimitive.Indicator.displayName;
 
+const NavigationListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, href, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          href={href || "#"}
+          ref={ref}
+          className={cn(
+            "block select-none group space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
+            "hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            // "border border-transparent hover:border-primary/10",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-base font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+NavigationListItem.displayName = "NavigationListItem";
+
 export {
   navigationMenuTriggerStyle,
   NavigationMenu,
+  NavigationListItem,
   NavigationMenuList,
   NavigationMenuItem,
   NavigationMenuContent,
