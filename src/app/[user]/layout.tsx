e158f55999c49ab94c7f1adb1d1b72d2bb17ddf7
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-07-20 12:30:23 -0400

    fix: profile 404

--- src/app/[user]/layout.tsx
+++ src/app/[user]/layout.tsx
@@ -1,13 +1,6 @@
 import { Metadata, ResolvingMetadata } from "next";
-import { notFound } from "next/navigation";
 import { getUserProfile } from "@/lib/queries/users";
-
-import Link from "next/link";
 import MarketingFooter from "@/components/core/MarketingFooter";
-import { SocialLinks } from "@/components/SocialLinks";
-import { SubNav } from "@/components/core/SubNav";
-import { Avatar } from "@/components/core/Avatar";
-import { getUserSession } from "@/lib/auth";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -28,7 +21,9 @@ export async function generateMetadata(
 
   // handle the 404 for no or invalid `product`
   if (!params.user || !profile) {
-    return notFound();
+    return {
+      title: "Profile Not Found" + " - Solfate",
+    };
   }
 
   let title = `@${profile.username}`;
@@ -52,88 +47,8 @@ export async function generateMetadata(
 }
 
 export default async function Layout({ children, params }: LayoutProps) {
-  if (!params.user) return notFound();
-
-  const session = await getUserSession();
-
-  // get the user's profile record from the database
-  const profile = await getUserProfile({
-    username: params.user,
-    // include: { people: true },
-  });
-
-  if (!profile) return notFound();
-
-  const isAuthedUserProfile =
-    session?.user.username.toLowerCase() == profile.username.toLowerCase();
-
   return (
     <>
-      <header className="border-b border-gray-300 bg-white">
-        <section className="container mx-auto max-w-5xl flex flex-col md:flex-row md:items-center justify-between gap-4 md:gap-2 !py-6 md:!py-10">
-          <div className="flex gap-3 md:gap-5 items-center flex-grow">
-            <Avatar
-              size={128}
-              username={profile.username}
-              title={profile.name || profile.username}
-              imageSrc={profile.image}
-              href={`/${profile.username}`}
-              className="rounded-full size-20 md:size-32"
-            />
-
-            <div className="flex-grow space-y-2 md:space-y-3">
-              <section className="space-y-0">
-                <h1 className="text-black line-clamp-1 text-xl md:text-3xl font-semibold">
-                  <Link href={`/${profile.username}`} className="">
-                    {profile.name || profile.username}
-                  </Link>
-                </h1>
-
-                <p className="text-gray-400 text-sm line-clamp-1 hidden md:block">
-                  @{profile.username}
-                </p>
-              </section>
-
-              {profile.oneLiner && (
-                <p className="text-gray-600 text-sm md:text-base line-clamp-2 md:line-clamp-1">
-                  {profile.oneLiner}
-                </p>
-              )}
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4 justify-between">
-            <SocialLinks
-              className={"order-1 md:order-2"}
-              twitter={profile.twitter}
-              github={profile.github}
-              website={profile.website}
-            />
-            {isAuthedUserProfile && (
-              <Link
-                href={"/settings/profile"}
-                className="btn btn-ghost order-2 md:order-1"
-              >
-                Edit Profile
-              </Link>
-            )}
-            {/* <button type="button" className="btn bg-hot-pink text-white">
-              Follow
-            </button> */}
-          </div>
-        </section>
-
-        <SubNav
-          className="container mx-auto max-w-5xl"
-          links={[
-            {
-              label: "Overview",
-              href: `/${profile.username}`,
-            },
-          ]}
-        />
-      </header>
-
       {children}
 
       <MarketingFooter />
--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -20,6 +20,10 @@ import Link from "next/link";
 import { DonateDialog } from "@/components/donate-dialog";
 import { Button } from "@/components/ui/button";
 
+import { SocialLinks } from "@/components/SocialLinks";
+import { SubNav } from "@/components/core/SubNav";
+import { Avatar } from "@/components/core/Avatar";
+
 type PageProps = {
   params: {
     user: string;
@@ -53,6 +57,9 @@ export default async function Page({ params }: PageProps) {
     return notFound();
   }
 
+  const isAuthedUserProfile =
+    session?.user.username.toLowerCase() == profile.username.toLowerCase();
+
   // get the mint timestamp and prevent data leakage
   const devlistMintTimestamp: number | undefined = await prisma.walletList
     .findUnique({
@@ -71,57 +78,64 @@ export default async function Page({ params }: PageProps) {
     });
 
   return (
-    <main className="container md:!py-12 min-h-[60vh] max-w-5xl gap-4 md:gap-y-10 flex flex-col md:flex-row justify-between">
-      <section className="min-w-80 md:max-w-80 md:order-2 flex flex-col gap-4">
-        {profile.walletAddress ? (
-          <DonateDialog walletAddress={profile.walletAddress} />
-        ) : isViewingOwnProfile ? (
-          <Card className="border-destructive border-dashed">
-            <CardHeader>
-              <CardTitle>
-                Start accepting tips and donations on Solana
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Button
-                asChild
-                variant={"destructive"}
-                className="block text-center"
-              >
-                <Link href={"/settings"}>Add a Public Wallet</Link>
-              </Button>
-            </CardContent>
-            <CardFooter className="!block space-y-1">
-              <p className="">
-                Add a public Solana wallet to accept tips and donations from
-                others directly to your wallet
-              </p>
-              <p className="text-muted-foreground text-sm italic">
-                Only you see this message
-              </p>
-            </CardFooter>
-          </Card>
-        ) : null}
+    <>
+      <ProfileHeader
+        profile={profile}
+        isAuthedUserProfile={isAuthedUserProfile}
+      />
 
-        {devlistMintTimestamp && (
-          <DevListCard mintTimestamp={devlistMintTimestamp} />
-        )}
-      </section>
+      <main className="container md:!py-12 min-h-[60vh] max-w-5xl gap-4 md:gap-y-10 flex flex-col md:flex-row justify-between">
+        <section className="min-w-80 md:max-w-80 md:order-2 flex flex-col gap-4">
+          {profile.walletAddress ? (
+            <DonateDialog walletAddress={profile.walletAddress} />
+          ) : isViewingOwnProfile ? (
+            <Card className="border-destructive border-dashed">
+              <CardHeader>
+                <CardTitle>
+                  Start accepting tips and donations on Solana
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Button
+                  asChild
+                  variant={"success"}
+                  className="block text-center"
+                >
+                  <Link href={"/settings/tips"}>Add a Public Wallet</Link>
+                </Button>
+              </CardContent>
+              <CardFooter className="!block space-y-1">
+                <p className="">
+                  Add a public Solana wallet to accept tips and donations from
+                  others directly to your wallet
+                </p>
+                <p className="text-muted-foreground text-sm italic">
+                  Only you see this message
+                </p>
+              </CardFooter>
+            </Card>
+          ) : null}
 
-      <section className="flex-grow md:order-1">
-        <ProfileSection
-          title={`About ${profile.name || `@${profile.username}`}`}
-        >
-          <p className="whitespace-pre break-words">
-            {profile.bio || profile.oneLiner || (
-              <span className="italic text-gray-400">
-                Nothing to see here :)
-              </span>
-            )}
-          </p>
-        </ProfileSection>
-      </section>
-    </main>
+          {devlistMintTimestamp && (
+            <DevListCard mintTimestamp={devlistMintTimestamp} />
+          )}
+        </section>
+
+        <section className="flex-grow md:order-1">
+          <ProfileSection
+            title={`About ${profile.name || `@${profile.username}`}`}
+          >
+            <p className="whitespace-pre break-words">
+              {profile.bio || profile.oneLiner || (
+                <span className="italic text-gray-400">
+                  Nothing to see here :)
+                </span>
+              )}
+            </p>
+          </ProfileSection>
+        </section>
+      </main>
+    </>
   );
 }
 
@@ -155,3 +169,76 @@ const DevListCard = ({ mintTimestamp }: { mintTimestamp: number }) => {
     </Card>
   );
 };
+
+const ProfileHeader = ({
+  profile,
+  isAuthedUserProfile,
+}: {
+  profile: NonNullable<Awaited<ReturnType<typeof getUserProfile>>>;
+  isAuthedUserProfile: boolean;
+}) => (
+  <header className="border-b border-gray-300 bg-white">
+    <section className="container mx-auto max-w-5xl flex flex-col md:flex-row md:items-center justify-between gap-4 md:gap-2 !py-6 md:!py-10">
+      <div className="flex gap-3 md:gap-5 items-center flex-grow">
+        <Avatar
+          size={128}
+          username={profile.username}
+          title={profile.name || profile.username}
+          imageSrc={profile.image}
+          href={`/${profile.username}`}
+          className="rounded-full size-20 md:size-32"
+        />
+
+        <div className="flex-grow space-y-2 md:space-y-3">
+          <section className="space-y-0">
+            <h1 className="text-black line-clamp-1 text-xl md:text-3xl font-semibold">
+              <Link href={`/${profile.username}`} className="">
+                {profile.name || profile.username}
+              </Link>
+            </h1>
+
+            <p className="text-gray-400 text-sm line-clamp-1 hidden md:block">
+              @{profile.username}
+            </p>
+          </section>
+
+          {profile.oneLiner && (
+            <p className="text-gray-600 text-sm md:text-base line-clamp-2 md:line-clamp-1">
+              {profile.oneLiner}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4 justify-between">
+        <SocialLinks
+          className={"order-1 md:order-2"}
+          twitter={profile.twitter}
+          github={profile.github}
+          website={profile.website}
+        />
+        {isAuthedUserProfile && (
+          <Link
+            href={"/settings/profile"}
+            className="btn btn-ghost order-2 md:order-1"
+          >
+            Edit Profile
+          </Link>
+        )}
+        {/* <button type="button" className="btn bg-hot-pink text-white">
+      Follow
+    </button> */}
+      </div>
+    </section>
+
+    <SubNav
+      className="container mx-auto max-w-5xl"
+      links={[
+        {
+          label: "Overview",
+          href: `/${profile.username}`,
+        },
+      ]}
+    />
+  </header>
+);
